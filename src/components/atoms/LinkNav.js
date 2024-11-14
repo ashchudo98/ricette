687fbc9eb3616a5a4573d8c9e734d25bd7a03acc
Ashley <ashley.chudory@soprasteria.com>
2024-11-14 17:24:02 +0100

    Fatto esplodere i props

--- src/components/atoms/LinkNav.js
+++ src/components/atoms/LinkNav.js
@@ -2,10 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "react-bootstrap/Nav";
 import Link from "next/link";
 
-export default function LinkNav(props) {
+export default function LinkNav({ href='', path='' }) {
   return (
-    <Nav.Link as={Link} href={props.href}>
-      {props.path}
+    <Nav.Link as={Link} href={href}>
+      {path}
     </Nav.Link>
   );
 }
--- src/components/atoms/LinkNavDropdown.js
+++ src/components/atoms/LinkNavDropdown.js
@@ -2,15 +2,15 @@ import { NavDropdown } from "react-bootstrap";
 import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function LinkNavDropdown(props) {
-  const dropDownItems = props.titles.map((_, i) => (
-    <NavDropdown.Item key={i} as={Link} href={props.href[i]}>
-      {props.titles[i - 1]}
+export default function LinkNavDropdown({ titles, href, title, id }) {
+  const dropDownItems = titles.map((_, i) => (
+    <NavDropdown.Item key={i} as={Link} href={href[i]}>
+      {titles[i - 1]}
     </NavDropdown.Item>
   ));
 
   return (
-    <NavDropdown title={props.title} id={props.id}>
+    <NavDropdown title={title} id={id}>
       {dropDownItems}
     </NavDropdown>
   );
