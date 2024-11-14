0b9831331bb87a6a61656b2de83306774e239a1e
Ashley <ashley.chudory@soprasteria.com>
2024-11-14 11:27:43 +0100

    Aggiunto una card per le ricette nella home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import NavBar from "@/components/organisms/NavBar"
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '@/firebase';
+import { Card } from "react-bootstrap";
 
+// Fetch data from Firestore
 const fetchData = async () => {
-  const querySnapshot = await getDocs(collection(db, 'ricette'));
+  const querySnapshot = await getDocs(collection(db, 'ricette'));  // Get all documents in the 'ricette' collection
   const ricetteData = querySnapshot.docs.map((doc) => ({
+    id: doc.id,  // Ensure each document has a unique ID
     nome: doc.data().nome,
     tempoPreparazione: doc.data().tempoPreparazione,
     unitaTempo: doc.data().unitaTempo,
@@ -15,34 +18,39 @@ const fetchData = async () => {
   return ricetteData;  
 };
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const data = await fetchData(params);
-  return { props: { data } }
+// Server-side props
+export async function getServerSideProps() {
+  const data = await fetchData();  // No need for params
+  return { props: { data } };
 }
 
+// Main component
 export default function Home({ data }) {
   const ricette = data;
+
   return (
     <div>
       <NavBar />
-      <h1>Home Page!</h1>
+      <h1 className="ms-5 mb-5">Home Page!</h1>
       <div>
-          {ricette.length === 0 ? (
-            <p>Nessuna ricetta trovata.</p>
-          ) : (
-            <div>
-              {ricette.map((ricetta) => (
-                <div key={ricetta.id}>
-                  <h3>{ricetta.nome}</h3>
-                  <h6>{ricetta.categoria}</h6>
-                  <h6>{ricetta.tempoPreparazione} {ricetta.unitaTempo}</h6>
-                  <p>{ricetta.descrizione}</p>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+        {ricette.length === 0 ? (
+          <p>Nessuna ricetta trovata.</p>
+        ) : (
+          <div>
+            {ricette.map((ricetta) => (
+              <Card key={ricetta.id} style={{ width: '50rem' }} className="mx-auto mt-3">  
+                <Card.Body>
+                  <Card.Title>{ricetta.nome}</Card.Title>
+                  <Card.Subtitle>
+                    {ricetta.categoria} | {ricetta.tempoPreparazione} {ricetta.unitaTempo}
+                  </Card.Subtitle> 
+                  <Card.Text>{ricetta.descrizione}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
