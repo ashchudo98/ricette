d5b462e85898aa2f099ce14587bef7a10c11530f
Ashley <ashley.chudory@soprasteria.com>
2024-11-14 11:30:46 +0100

    Aggiunto card pure per le ricette delle categorie

--- src/pages/categorie/[categorie].js
+++ src/pages/categorie/[categorie].js
@@ -4,6 +4,7 @@ import NavBar from '@/components/organisms/NavBar';
 import FormAddRicetta from '@/components/organisms/FormAddRicetta';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/firebase';
+import { Card } from 'react-bootstrap';
 
 const fetchData = async (params) => {
   const q = query(collection(db, 'ricette'), where('categoria', '==', params.categorie));
@@ -47,6 +48,31 @@ export default function Page({ data }) {
   return (
     <div>
       <NavBar />
+      <h1 className="ms-5 mb-5">{router.query.categorie}</h1>
+      <div>
+        {ricette.length === 0 ? (
+          <p>Nessuna ricetta trovata.</p>
+        ) : (
+          <div>
+            {ricette.map((ricetta) => (
+              <Card key={ricetta.id} style={{ width: '50rem' }} className="mx-auto mt-3">  
+                <Card.Body>
+                  <Card.Title>{ricetta.nome}</Card.Title>
+                  <Card.Subtitle>{ricetta.tempoPreparazione} {ricetta.unitaTempo}
+                  </Card.Subtitle> 
+                  <Card.Text>{ricetta.descrizione}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+
+  /*return (
+    <div>
+      <NavBar />
       <h1>{router.query.categorie}</h1>
         <div>
           {ricette.length === 0 ? (
@@ -64,5 +90,5 @@ export default function Page({ data }) {
           )}
         </div>
     </div>
-  );
+  );*/
 }
